18e2eca254250326c4a6a6075e027f6dde7cbda4
Cauerio <145791894+Cauerio@users.noreply.github.com>
2024-02-08 18:47:02 +0100

    Add files via upload

--- django_proyecto/contrasenagestor/urls.py
+++ django_proyecto/contrasenagestor/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('create/', views.create_pass, name='create_pass'),
     path('show/', views.show_pass, name='show_pass'),
-    path('change/<int:id_usuario>/', views.chang_pass, name='change_pass'),
-    path('delete/<int:id_usuario>/', views.del_pass, name='delete_pass'),
+    path('change/', views.chang_pass, name='change_pass'),
+    path('delete/', views.del_pass, name='delete_pass'),
 ]
--- django_proyecto/contrasenagestor/views.py
+++ django_proyecto/contrasenagestor/views.py
@@ -1,39 +1,53 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.core import serializers
 from .models import Datos
 from django.views.decorators.csrf import csrf_exempt
-
-
-def index(request):
-    print("Hello")
-    return HttpResponse("Hello from index view")
+import json
 
 @csrf_exempt
 def create_pass(request):
-    # Recupera los datos de la URL
-    usuario = request.GET.get('usuario', '')
-    password = request.GET.get('password', '')
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        usuario = data.get('usuario', '')
+        password = data.get('password', '')
 
-    # Crea una nueva instancia de Datos y guarda en la base de datos
-    Datos.objects.create(usuario=usuario, password=password)
+        if not usuario or not password:
+            return HttpResponseBadRequest("Se necesita 'usuario' y 'password'.")
+
+        try:
+            Datos.objects.create(usuario=usuario, password=password)
+            return HttpResponse("Usuario creado")
+        except:
+            return HttpResponseBadRequest("Error al crear la password.")
+    else:
+        return HttpResponseBadRequest("Solo se acepta post.")
     
-    return HttpResponse("Password created successfully")
     
 @csrf_exempt
 def show_pass(request):
-    # Aquí deberías manejar la lógica para mostrar todas las contraseñas
     data = serializers.serialize('json', Datos.objects.all())
     return HttpResponse(data, content_type='application/json')
 
+
+    
 @csrf_exempt
-def del_pass(request, usuario, password):
-    # Aquí deberías manejar la lógica para eliminar una contraseña por su ID
-    Datos.objects.filter(usuario=usuario, password=password).delete()
-    return HttpResponse("Password deleted successfully")
+def del_pass(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        id_usuario = data.get('id_usuario')
+        try:
+            Datos.objects.filter(id_usuario=id_usuario).delete()
+            return HttpResponse("Usuario borrado.")
+        except Exception as e:
+            return HttpResponseBadRequest(f"Fallo al borrar el usuario {str(e)}")
+    else:
+        return HttpResponseBadRequest("Fallo")
+
+
+
 
 @csrf_exempt
-def chang_pass(request, id_usuario):
-    # Aquí deberías manejar la lógica para cambiar la contraseña por su ID
-    Datos.objects.filter(id_usuario=id_usuario).update(password='nueva_contraseña')
-    return HttpResponse("Password changed successfully")
+def chang_pass(request, id_usuario, usuario, password):
+    Datos.objects.filter(id_usuario=id_usuario, usuario=usuario, password=password).update(password='nueva_contraseña')
+    return HttpResponse("Password cambiado")
